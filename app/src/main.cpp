98e403dd12c1e1529203e89b7f5c188b0fdd890a
Mathias Otnes <Mathias.otnes@gmail.com>
2022-10-28 16:43:37 +0200

    fikset make

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,12 +1,46 @@
 /* Imported libaries */
+#ifdef __APPLE__
+#define GL_SILENCE_DEPRECATION
+#endif
 #include <iostream> 
+#include "../inc/glfw3.h"
 
 /* User libaries */
 #include "../inc/main.h"
 #include "../inc/api.h"
 
-int main() {
-    std::string str = API::hello_world();
-    std::cout << str << std::endl;
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
